55be18dca7384e60d900756ab67132ac89c0b195
NicolasLaurin <54424767+NicolasLaurin@users.noreply.github.com>
2021-11-15 16:32:01 -0500

    Added RFID authentication (Pseudo code)

---
+++ authentication_page.py
@@ -0,0 +1,17 @@
+#---THIS IS THE FIRST PAGE THE USER WILL SEE WHEN STARTING THE APPLICATION---
+
+
+
+
+
+
+
+
+#Add a small UI to this page that asks the user to tap their RFID tag onto to the RFID reader.
+
+##If they are a valid user, then proceed.
+    ###Display Home dashboard
+        ####with "Welcome User" (Default: "Welcome User" | If user's name is Sebastian: "Welcome Sebastian")
+        ####and set the favorite parameters as the default thresholds.
+        ####Send email “ At hh:mm time, this user is here.”
+##else if they are not a valid user, enable the alarm (buzzer) and display "Access Denied".
